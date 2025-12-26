5d3467d6ab0f8a4ad81c49f0c6284940169c3688
Rk Roy🇳🇵 <rajr86642@gmail.com>
2025-12-26 14:31:31 +0530

    Add : Structured Output using Pydantic & Json Schema Based Examples in LangChain

--- LangChain & LangGraph/LangChain/Introduction & Fundamentals/Models/LLM_DEMO1.py
+++ LangChain & LangGraph/LangChain/Introduction & Fundamentals/Models/LLM_DEMO1.py
@@ -9,6 +9,10 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+# OpenRouter
+# llm = ChatOpenAI(model="gpt-3.5-turbo")
+# llm = ChatOpenAI(model="deepseek/deepseek-r1-0528:free")
+
 # Initialize the Gemini language model (version 2.5 flash)
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
