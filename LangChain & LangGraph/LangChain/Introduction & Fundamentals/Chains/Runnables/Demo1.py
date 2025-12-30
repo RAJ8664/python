887425083c0ef47e1d3a59d8083c22259ab15cca
Rk Roy🇳🇵 <rajr86642@gmail.com>
2025-12-30 15:50:12 +0530

    Add : Runnable Sequence

--- LangChain & LangGraph/LangChain/Introduction & Fundamentals/Chains/Runnables/Demo1.py
+++ LangChain & LangGraph/LangChain/Introduction & Fundamentals/Chains/Runnables/Demo1.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm = ChatOpenAI(model="gpt-3.5-turbo")
+parser = StrOutputParser()
 
 
 def addTwo(x: int):
@@ -17,6 +18,22 @@ def square(x: int):
     return x**2
 
 
+def getReport(topic: str):
+    template = PromptTemplate(
+        template="Give me a brief report on topic : {topic}", input_variables=["topic"]
+    )
+    formatted_prompt = template.format(topic=topic)
+    return llm.invoke(formatted_prompt)
+
+
+def getSummary(topic: str):
+    template = PromptTemplate(
+        template="Give me a brief summary of topic : {topic}", input_variables=["topic"]
+    )
+    res = llm.invoke(template.format(topic=topic))
+    return res
+
+
 chain1 = RunnableLambda(addTwo) | RunnableLambda(square)
 
 chain2 = RunnableLambda(addTwo) | {
@@ -25,6 +42,10 @@ chain2 = RunnableLambda(addTwo) | {
     "mul3": RunnableLambda(lambda x: x * 4),
 }
 
+chain3 = RunnableLambda(getReport) | RunnableLambda(getSummary)
 
 print(chain1.invoke(1))  # 9
 print(chain2.invoke(1))  # {'mul1': 6, 'mul2': 9, 'mul3': 12}
+print(
+    chain3.invoke("blockchain")
+)  # Brief report on topic : blockchain with parsed output.
